57c50a439bcead90dbcf01b670bf47d88cac9afa
Bharathajxworkz <bharathaj2211.xwokz@gmail.com>
2021-12-20 23:17:44 +0530

    add the file Boom

---
+++ Boom.java
@@ -0,0 +1,18 @@
+class Boom{
+	public static void main(String[] args){
+
+		double[] points = {9.01255833,8.26255896,9.232596,3.1424655,3.1459720,9.9715411121,99.236852212,96.785658755000,9.254155111,6.981145221,9.74777774551};
+	System.out.println(points[0]);
+	System.out.println(points[1]);
+	System.out.println(points[2]);
+	System.out.println(points[3]);
+	System.out.println(points[4]);
+	System.out.println(points[5]);
+	System.out.println(points[6]);
+	System.out.println(points[7]);
+	System.out.println(points[8]);
+	System.out.println(points[9]);
+	System.out.println(points[10]);
+
+}
+}
